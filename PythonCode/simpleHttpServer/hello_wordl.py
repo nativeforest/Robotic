0b3f45d2d7ddd90925258a7c41d865bdd62acfa1
emauriciorg <emauriciorg@gmail.com>
2016-09-02 22:07:53 -0500

    Simple http server

---
+++ PythonCode/simpleHttpServer/hello_wordl.py
@@ -0,0 +1,13 @@
+
+import SimpleHTTPServer
+import SocketServer
+    
+PORT = 8000
+    
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+Handler.extensions_map.update({'.webapp': 'application/x-web-app-manifest+json',});
+ 
+httpd = SocketServer.TCPServer(("192.168.1.43", PORT), Handler)
+#aqui va la direccion local, con el comando ifconfig la saca, 192.168.1.43:8000
+print "Serving at port", PORT
+httpd.serve_forever()
